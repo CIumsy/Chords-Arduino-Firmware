617fdbae10630ca89788cf4bab54287e4f7e54c4
Krishnanshu Mittal <karan4g79@gmail.com>
2025-07-28 16:59:33 +0530

    Added credits

--- ESP32-S3/ESP32-S3.ino
+++ ESP32-S3/ESP32-S3.ino
@@ -11,8 +11,9 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 //
-// Copyright (c) 2024 - 2025 Upside Down Labs - contact@upsidedownlabs.tech
-// Author: Deepak Khatri
+// Copyright (c) 2025 Krishnanshu Mittal - krishnanshu@upsidedownlabs.tech
+// Copyright (c) 2025 Deepak Khatri - deepak@upsidedownlabs.tech
+// Copyright (c) 2025 Upside Down Labs - contact@upsidedownlabs.tech
 //
 // At Upside Down Labs, we create open-source DIY neuroscience hardware and software.
 // Our mission is to make neuroscience affordable and accessible for everyone.
@@ -23,31 +24,29 @@
 #include <sdkconfig.h>
 
 // ----- Configuration -----
-#define TIMER_FREQ        1000000UL          // 1 MHz timer tick
-#define NUM_CHANNELS      16
-#define HEADER_LEN        3
-#define PACKET_LEN       (NUM_CHANNELS*2 + HEADER_LEN + 1)
-#define SAMP_RATE        500.0               // 500 samples/sec
-#define SYNC_BYTE_1     0xC7
-#define SYNC_BYTE_2     0x7C
-#define END_BYTE        0x01
-#define BAUD_RATE     230400
+#define TIMER_FREQ 1000000UL  // 1 MHz timer tick
+#define NUM_CHANNELS 16
+#define HEADER_LEN 3
+#define PACKET_LEN (NUM_CHANNELS * 2 + HEADER_LEN + 1)
+#define SAMP_RATE 500.0  // 500 samples/sec
+#define SYNC_BYTE_1 0xC7
+#define SYNC_BYTE_2 0x7C
+#define END_BYTE 0x01
+#define BAUD_RATE 230400
 
-const uint8_t adcPins[NUM_CHANNELS] = {1, 11, 2, 12, 3, 13, 4, 14, 5, 15, 6, 16, 7, 17, 8, 18};
+const uint8_t adcPins[NUM_CHANNELS] = { 1, 11, 2, 12, 3, 13, 4, 14, 5, 15, 6, 16, 7, 17, 8, 18 };
 
 // Global constants and variables
-uint8_t packetBuffer[PACKET_LEN]; // The transmission packet
-uint8_t currentChannel;           // Current channel being sampled
-uint16_t adcValue = 0;            // ADC current value
-bool timerStatus = false;         // Timer status bit
-bool bufferReady = false;         // Buffer ready status bit
+uint8_t packetBuffer[PACKET_LEN];  // The transmission packet
+uint8_t currentChannel;            // Current channel being sampled
+uint16_t adcValue = 0;             // ADC current value
+bool timerStatus = false;          // Timer status bit
+bool bufferReady = false;          // Buffer ready status bit
 
 hw_timer_t *timer_1 = NULL;
 
-void IRAM_ATTR ADC_ISR()
-{
-  if (!timerStatus or Serial.available())
-  {
+void IRAM_ATTR ADC_ISR() {
+  if (!timerStatus or Serial.available()) {
     timerStop();
     return;
   }
@@ -56,34 +55,30 @@ void IRAM_ATTR ADC_ISR()
   bufferReady = true;
 }
 
-void timerStart()
-{
+void timerStart() {
   timerStatus = true;
   timerStart(timer_1);
 }
 
-void timerStop()
-{
+void timerStop() {
   timerStatus = false;
   bufferReady = false;
   timerStop(timer_1);
 }
 
-void setup()
-{
+void setup() {
 
   Serial.begin(BAUD_RATE);
   Serial.setTimeout(100);
-  while (!Serial)
-  {
-    ; // Wait for serial port to connect. Needed for native USB
+  while (!Serial) {
+    ;  // Wait for serial port to connect. Needed for native USB
   }
 
   // Initialize packetBuffer
-  packetBuffer[0] = SYNC_BYTE_1;           // Sync 0
-  packetBuffer[1] = SYNC_BYTE_2;           // Sync 1
-  packetBuffer[2] = 0;                     // Packet counter
-  packetBuffer[PACKET_LEN - 1] = END_BYTE; // End Byte
+  packetBuffer[0] = SYNC_BYTE_1;            // Sync 0
+  packetBuffer[1] = SYNC_BYTE_2;            // Sync 1
+  packetBuffer[2] = 0;                      // Packet counter
+  packetBuffer[PACKET_LEN - 1] = END_BYTE;  // End Byte
 
   timer_1 = timerBegin(1000000);
   timerAttachInterrupt(timer_1, &ADC_ISR);
@@ -92,19 +87,16 @@ void setup()
   analogReadResolution(12);
 }
 
-void loop()
-{
+void loop() {
   // Send data if the buffer is ready and the timer is activ
-  if (timerStatus and bufferReady)
-  {
+  if (timerStatus and bufferReady) {
 
     // ADC value Reading, Converting, and Storing:
-    for (currentChannel = 0; currentChannel < NUM_CHANNELS; currentChannel++)
-    {
+    for (currentChannel = 0; currentChannel < NUM_CHANNELS; currentChannel++) {
       adcValue = analogRead(adcPins[currentChannel]);
       // Store current values in packetBuffer to send.
-      packetBuffer[((2 * currentChannel) + HEADER_LEN)] = highByte(adcValue);    // Write High Byte
-      packetBuffer[((2 * currentChannel) + HEADER_LEN + 1)] = lowByte(adcValue); // Write Low Byte
+      packetBuffer[((2 * currentChannel) + HEADER_LEN)] = highByte(adcValue);     // Write High Byte
+      packetBuffer[((2 * currentChannel) + HEADER_LEN + 1)] = lowByte(adcValue);  // Write Low Byte
     }
 
     // Increment the packet counter
@@ -115,30 +107,24 @@ void loop()
     bufferReady = false;
   }
 
-  if (Serial.available())
-  {
+  if (Serial.available()) {
     String command = Serial.readStringUntil('\n');
-    command.trim();        // Remove extra spaces or newline characters
-    command.toUpperCase(); // Normalize to uppercase for case-insensitivity
+    command.trim();         // Remove extra spaces or newline characters
+    command.toUpperCase();  // Normalize to uppercase for case-insensitivity
 
-    if (command == "WHORU") // Who are you?
+    if (command == "WHORU")  // Who are you?
     {
       Serial.println("ESP32-S3");
-    }
-    else if (command == "START") // Start data acquisition
+    } else if (command == "START")  // Start data acquisition
     {
       timerStart();
-    }
-    else if (command == "STOP") // Stop data acquisition
+    } else if (command == "STOP")  // Stop data acquisition
     {
       timerStop();
-    }
-    else if (command == "STATUS") // Get status
+    } else if (command == "STATUS")  // Get status
     {
       Serial.println(timerStatus ? "RUNNING" : "STOPPED");
-    }
-    else
-    {
+    } else {
       Serial.println("UNKNOWN COMMAND");
     }
   }
